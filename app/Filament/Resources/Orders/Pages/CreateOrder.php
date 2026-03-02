c51f74ed7652c1b3a040aeb3ec8d3e26026a6dc9
No-Sunlight <theendlesssunset@gmail.com>
2026-03-02 15:31:46 -0600

    Modal de confirmacioOrdenn. Requiere detalles

--- app/Filament/Resources/Orders/Pages/CreateOrder.php
+++ app/Filament/Resources/Orders/Pages/CreateOrder.php
@@ -7,6 +7,7 @@ use App\Models\Customer;
 use App\Models\CustomerCoupon;
 use App\Models\OrderDetail;
 use App\Models\Product;
+use Filament\Actions\Action;
 use Filament\Resources\Pages\CreateRecord;
 use Illuminate\Database\Eloquent\Model;
 use Tiptap\Nodes\DetailsSummary;
@@ -14,6 +15,26 @@ use Tiptap\Nodes\DetailsSummary;
 class CreateOrder extends CreateRecord
 {
     protected static string $resource = OrderResource::class;
+
+
+
+
+    protected function getCreateFormAction(): Action
+    {
+        return parent::getCreateFormAction()
+        ->submit(form:null)
+        ->requiresConfirmation()
+        ->modalDescription("Confimrar pedido")
+        ->action(function ()
+        {
+            $this->closeActionModal();
+            $this->cteate();
+        });
+        //
+
+    }
+
+    
 
 
      protected function mutateFormDataBeforeCreate(array $data): array
@@ -29,13 +50,13 @@ class CreateOrder extends CreateRecord
 
          protected function afterCreate(): void
     {
-        //Este es el metodo correcto para obtener el ultimo registro guardado
-        $record = $this->record; //Debo de recordarlo para futuras referencias
-        $orderdetails = OrderDetail::where('order_id','=',$record->id)->get();
+            //Este es el metodo correcto para obtener el ultimo registro guardado
+            $record = $this->record; //Debo de recordarlo para futuras referencias
+            $orderdetails = OrderDetail::where('order_id','=',$record->id)->get();
 
-        //Un cliente registrado ha hecho una orden en presencial, asi que es necesario añadir a sus puntos
-        //Quise dividirlo para economizar el proceso de añadir puntos y que no se confundiera con el de un cliente no registrado
-        if(!is_null($record->customer_id))
+            //Un cliente registrado ha hecho una orden en presencial, asi que es necesario añadir a sus puntos
+            //Quise dividirlo para economizar el proceso de añadir puntos y que no se confundiera con el de un cliente no registrado
+            if(!is_null($record->customer_id))
             {
                     $points=0;
                     $customer = Customer::find($record->customer_id);
--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -29,6 +29,8 @@ class AdminPanelProvider extends PanelProvider
             ->id('admin')
             ->path('admin')
             ->login()
+             ->brandName("D'Gary-O")
+
             ->colors([
                 'primary' => Color::Blue,
             ])
