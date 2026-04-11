6fc22d275ee8cd12aea77c19b32c99d76e4cfe18
Grxson <gael.grxson@gmail.com>
2026-04-10 19:48:37 -0600

    Fix: Update database password for production environment

--- config/database.php
+++ config/database.php
@@ -50,7 +50,7 @@ return [
             'port' => env('DB_PORT', env('APP_ENV') === 'production' ? '14830' : '3306'),
             'database' => env('DB_DATABASE', env('APP_ENV') === 'production' ? 'railway' : 'laravel'),
             'username' => env('DB_USERNAME', 'root'),
-            'password' => env('DB_PASSWORD', ''),
+            'password' => env('DB_PASSWORD', 'dLjSkztMpQNpbdJHGEmtjbMeTgFZjnwb'),
             'unix_socket' => env('DB_SOCKET', ''),
             'charset' => env('DB_CHARSET', 'utf8mb4'),
             'collation' => env('DB_COLLATION', 'utf8mb4_unicode_ci'),
