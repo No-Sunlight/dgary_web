a5a5afcf40265fb1848275decd919b922c6e1395
Grxson <gael.grxson@gmail.com>
2026-04-10 20:59:14 -0600

    configure trusted proxies for HTTPS

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -13,6 +13,7 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware): void {
+        $middleware->trustProxies(at: '*');
         $middleware->alias([
             'api.token' => \App\Http\Middleware\ValidateApiToken::class,
         ]);
