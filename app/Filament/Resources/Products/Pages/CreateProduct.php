222ffe0d8f7ff7eccfe0b01d2776b0694beffdfc
No-Sunlight <theendlesssunset@gmail.com>
2026-04-11 20:46:43 -0600

    ProductsResource error Stock_Counts modelo deprecated

--- app/Filament/Resources/Products/Pages/CreateProduct.php
+++ app/Filament/Resources/Products/Pages/CreateProduct.php
@@ -3,19 +3,11 @@
 namespace App\Filament\Resources\Products\Pages;
 
 use App\Filament\Resources\Products\ProductResource;
-use App\Models\StockCount;
 use Filament\Resources\Pages\CreateRecord;
 
 class CreateProduct extends CreateRecord
 {
     protected static string $resource = ProductResource::class;
-
-
-    protected function afterCreate(): void {
-           $record = $this->record;
-           $stock_record = new StockCount();
-           $stock_record->product_id=$record->id;
-           $stock_record->save();
-
-         }
+    //Se elimino referencia a stockcounts: Deprecated, el modelo ya no existe.
+  
 }
