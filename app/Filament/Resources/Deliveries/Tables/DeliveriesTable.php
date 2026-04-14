f86047988cdff981af70b756dc1ab63e87253bca
Diego Castaneda <cardsm713@gmail.com>
2026-04-14 03:19:55 -0600

    Correccion en action de deliverie table, solo asign repartidor y no los otros

--- app/Filament/Resources/Deliveries/Tables/DeliveriesTable.php
+++ app/Filament/Resources/Deliveries/Tables/DeliveriesTable.php
@@ -4,14 +4,18 @@ namespace App\Filament\Resources\Deliveries\Tables;
 
 use App\Models\Delivery;
 use App\Models\Driver;
+use BladeUI\Icons\Components\Icon;
 use Filament\Actions\Action;
 use Filament\Actions\BulkActionGroup;
 use Filament\Actions\DeleteBulkAction;
 use Filament\Actions\EditAction;
 use Filament\Actions\ViewAction;
 use Filament\Forms\Components\Select;
+use Filament\Forms\Components\TextInput;
+use Filament\Infolists\Components\TextEntry;
 use Filament\Support\Icons\Heroicon;
 use Filament\Tables\Columns\TextColumn;
+use Filament\Tables\Filters\SelectFilter;
 use Filament\Tables\Table;
 use Illuminate\Database\Eloquent\Model;
 
@@ -21,7 +25,7 @@ class DeliveriesTable
     {
         return $table
             ->columns([
-                TextColumn::make('user.name')
+                TextColumn::make('driver.name')
                     ->numeric()
                     ->label("Repartidor")
                     ->sortable(),
@@ -33,20 +37,59 @@ class DeliveriesTable
                     ->numeric()
                     ->sortable(),
                 TextColumn::make('created_at')
+                    ->label("Creada")
                     ->dateTime()
                     ->sortable()
                     ->toggleable(isToggledHiddenByDefault: true),
                 TextColumn::make('updated_at')
+                    ->label('Actualizada')
                     ->dateTime()
                     ->sortable()
                     ->toggleable(isToggledHiddenByDefault: true),
             ])
             ->filters([
-                //
+                SelectFilter::make('status')
+                    ->options([
+                        'pending' => 'pending',
+                        'ready' => 'ready',
+                        'in_transit' => 'in_transit',
+                        'completed' => 'completed'
+                    ]),
+
+
             ])
             ->recordActions([
-                //EditAction::make(),
-                ViewAction::make(),
+
+                ViewAction::make()
+
+                ,
+
+                //Cancelar
+                Action::make('cancel')
+                    ->label('Cancelar')
+                    ->modalHeading('Cancelar orden')
+                    ->color('danger')
+                    ->icon(Heroicon::ExclamationTriangle)
+                    ->modalDescription('ADVERTENCIA: UNA VEZ CANCELADA LA ORDEN, NO PUEDE SER CAMBIADA')
+                    ->action(function ($data, Model $record) {
+                        $record->status = 'canceled';
+                        $record->save();
+                    }),
+
+
+
+                //Indicar que la orden esta lista
+                Action::make('pick-up_ready')
+                    ->icon(Heroicon::Bell)
+                    ->label('Lista')
+                    ->visible(fn($record) => $record->status == 'pending')
+                    ->modalHeading('Cambiar el estatus de la orden a "Lista"')
+                    ->modalDescription('Solo cambie el estado si la orden esta lista para ser recogida por el repartidor')
+                    ->action(function ($data, Model $record) {
+                        $record->status = 'ready';
+                        $record->save();
+                    }),
+                //Accion de asignar un repartidor.
                 Action::make("assign_driver")
                     ->label('Asignar')
                     ->color('success')
